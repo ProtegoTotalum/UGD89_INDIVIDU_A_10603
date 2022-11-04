714f92af550f1f64b2f4b414c3ebc2995de4dd9f
GrayfienHalim <grayfien2002@gmail.com>
2022-11-04 22:04:14 +0700

    Project 2

---
+++ app/src/main/java/com/grayfien/modul89_a_10603_project2/NotificationReceiver.kt
@@ -0,0 +1,13 @@
+package com.grayfien.modul89_a_10603_project2
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class NotificationReceiver  : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent) {
+        val message = intent.getStringExtra("toastMessage")
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+}
